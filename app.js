3dde1e55d6e26dd2e377939bc907d271014dca5b
= <=>
2024-07-16 13:31:25 +0530

    added css file from agela

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
-var data = "Hello World!";
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = [];
 
 app.get("/", (req, res) => {
-  res.render("list", { data: data });
+  var date = new Date();
+
+  var options = {
+    month: "short",
+    day: "2-digit",
+    weekday: "long",
+  };
+  var day = date.toLocaleDateString("en", options);
+  res.render("list", { data: day, items: items });
+});
+
+app.post("/", (req, res) => {
+  var item = req.body.todo;
+  if (item) items.push(item);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
